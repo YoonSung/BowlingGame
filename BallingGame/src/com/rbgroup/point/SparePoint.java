5de43a11f55af3468f5ee3ce7b6d50d8215ad487
Yoon <lvev9925@naver.com>
2013-08-25 20:21:21 +0900

    Make getPoint() Method, Only Operated in SingleFrame (restricted).

--- BallingGame/src/com/rbgroup/point/SparePoint.java
+++ BallingGame/src/com/rbgroup/point/SparePoint.java
@@ -2,15 +2,29 @@ package com.rbgroup.point;
 
 public class SparePoint extends Point {
 
+	boolean isSpareCalculated = false;
+	private Type type;
+	
+	
 	public SparePoint(int shootNumber) {
 		super(shootNumber, Type.SPARE);
-		Type type = getType();
+		type = getType();
 		type.setPoint(shootNumber);
 	}
 
 	@Override
 	public int getPoint() {
-		// TODO Auto-generated method stub
+		if (isSpareCalculated)
+			return type.getPoint();
 		return 0;
+	}
+	
+	public void addPoint(int extraPoint) throws Exception{
+		if (isSpareCalculated)
+			throw new Exception("Invalid Access to Caculated Point");
+		
+		int sparePinNumber = type.getPoint();
+		type.setPoint(sparePinNumber+extraPoint);
+		isSpareCalculated = true;
 	}
 }
--- BallingGame/test/com/rbgroup/point/SparePointTest.java
+++ BallingGame/test/com/rbgroup/point/SparePointTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.CoreMatchers.*;
 import org.junit.Before;
 import org.junit.Test;
 import com.rbgroup.point.Point.Type;
+import com.rbgroup.score.Score;
 
 public class SparePointTest {
 	
@@ -20,5 +21,13 @@ public class SparePointTest {
 		assertThat(instance.getDownPinNumber(), is(4));
 		assertThat(instance.getLetter(), is('/'));
 		assertThat(instance.getType(), is(Type.SPARE));
+	}
+	
+	@Test
+	public void getOneFramePoint() {
+		Score score = new Score();
+		score.addScore(new PlainNumberPoint(3));
+		score.addScore(new SparePoint(7));
+		assertThat(score.getScore(), is(3));
 	}
 }
